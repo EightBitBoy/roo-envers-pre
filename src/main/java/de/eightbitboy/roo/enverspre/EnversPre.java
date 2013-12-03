8c166ea6c094b4c04d5aea887df6065168a3a3ca
Stefan Haertel <haertel.dev@gmail.com>
2013-12-03 13:52:07 +0100

    getting things started

--- src/main/java/de/eightbitboy/roo/enverspre/EnversPre.java
+++ src/main/java/de/eightbitboy/roo/enverspre/EnversPre.java
@@ -1,9 +1,16 @@
 package de.eightbitboy.roo.enverspre;
 
-import de.eightbitboy.roo.enverspre.Database;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
 
-public class EnversPre {
-	public static void main(String[] args) {
-		Database.setup();
+public class EnversPre{
+	public static void main(String[] args){
+		Configuration configuration = new Configuration();
+		configuration.configure();
+		/*
+		ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(settings);
+		*/
 	}
 }
--- src/main/java/de/eightbitboy/roo/enverspre/data/Animal.java
+++ src/main/java/de/eightbitboy/roo/enverspre/data/Animal.java
@@ -1,5 +1,28 @@
 package de.eightbitboy.roo.enverspre.data;
 
 public class Animal{
-	public String name;
+	private long id;
+	private String name;
+	
+	public Animal(){};
+	
+	public Animal(String name){
+		this.name = name;
+	}
+	
+	public long getId(){
+		return this.id;
+	}
+	
+	public void setId(long id){
+		this.id = id;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
 }
